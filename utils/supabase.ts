66c39f9820ba4e22fcd2799ad50e664c858c7390
Jose Alexei Garcia <darkspikyshark@gmail.com>
2026-04-21 11:48:23 +0800

    fix: Replace expo-secure-store for localStorage

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,12 +1,24 @@
-import 'expo-sqlite/localStorage/install'
 import { createClient } from '@supabase/supabase-js'
+import * as SecureStore from 'expo-secure-store'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 
+const supabaseStorage = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key)
+    },
+    setItem: (key: string, value: string) => {
+        return SecureStore.setItemAsync(key, value)
+    },
+    removeItem: (key: string) => {
+        return SecureStore.deleteItemAsync(key)
+    },
+}
+
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
     auth: {
-        storage: localStorage,
+        storage: supabaseStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
