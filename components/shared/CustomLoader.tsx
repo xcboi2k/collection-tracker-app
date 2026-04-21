16d3d4e0d90f1fdcc51a1f8119634273291d8c78
Jose Alexei Garcia <darkspikyshark@gmail.com>
2026-04-21 12:24:34 +0800

    fix: Adjusted styling to cover whole screen

--- components/shared/CustomLoader.tsx
+++ components/shared/CustomLoader.tsx
@@ -6,17 +6,13 @@ import colors from '@/assets/themes/colors'
 const CustomLoader = ({ visible }) => {
     return (
         <Modal transparent visible={visible} animationType="fade">
-            {/* Fullscreen container */}
-            <View className="flex-1 justify-center items-center">
-                {/* Overlay */}
-                <View className="absolute inset-0 bg-black/50 justify-center items-center">
-                    {/* Loader box */}
-                    <View className="p-5 rounded-lg bg-transparent">
-                        <ActivityIndicator
-                            size="large"
-                            color={colors.primary.colorOne} // replace with colors.primary.colorOne
-                        />
-                    </View>
+            {/* Full screen overlay */}
+            <View className="flex-1 bg-black/50 justify-center items-center">
+                <View className="p-5 rounded-lg">
+                    <ActivityIndicator
+                        size="large"
+                        color={colors.primary.colorOne}
+                    />
                 </View>
             </View>
         </Modal>
