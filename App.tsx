2eebd2b44b5ee3dfdd783d19588e941a25533396
Jose Alexei Garcia <darkspikyshark@gmail.com>
2026-04-21 11:48:40 +0800

    fix: Sentry initialization

--- App.tsx
+++ App.tsx
@@ -10,6 +10,13 @@ import MainMenuScreen from './components/screens/main/MainMenuScreen'
 import MainApp from './components/MainApp'
 import SignUpScreen from './components/screens/auth/SignUpScreen'
 
+Sentry.init({
+    dsn: 'https://6fc20bd1b053ec6ca53c66ff6c386cc2@o4511221589475328.ingest.us.sentry.io/4511255663083520',
+    // Adds more context data to events (IP address, cookies, user, etc.)
+    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+    sendDefaultPii: true,
+})
+
 function App() {
     return (
         <>
