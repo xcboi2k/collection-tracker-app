e90a978c5fc1bad9abbf66218eb0080984d72135
Jose Alexei Garcia <darkspikyshark@gmail.com>
2026-04-23 10:02:09 +0800

    fix: Replaced with AsyncStorage

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,24 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
-import * as SecureStore from 'expo-secure-store'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 
-const supabaseStorage = {
-    getItem: (key: string) => {
-        return SecureStore.getItemAsync(key)
-    },
-    setItem: (key: string, value: string) => {
-        return SecureStore.setItemAsync(key, value)
-    },
-    removeItem: (key: string) => {
-        return SecureStore.deleteItemAsync(key)
-    },
-}
-
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
     auth: {
-        storage: supabaseStorage,
+        storage: AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
