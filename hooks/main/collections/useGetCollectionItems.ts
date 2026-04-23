d113bb522967f31f83eba6c08e90c0b39d8f312f
Jose Alexei Garcia <darkspikyshark@gmail.com>
2026-04-23 09:59:14 +0800

    fix: Added setting of collectionItems state

--- hooks/main/collections/useGetCollectionItems.ts
+++ hooks/main/collections/useGetCollectionItems.ts
@@ -4,12 +4,19 @@ import * as Sentry from '@sentry/react-native'
 import { supabase } from '@/utils/supabase'
 import { showToast } from '@/utils/showToast'
 import { cleanAuthError } from '@/utils/errors/cleanError'
+import CollectionStore from '@/stores/CollectionStore'
+import UserStore from '@/stores/UserStore'
 
 export default function useGetCollectionItems() {
+    const setCollectionItems = CollectionStore(
+        (state) => state.setCollectionItems
+    )
+    const user = UserStore((state) => state.user)
+
     const [loading, setLoading] = useState(false)
     const [data, setData] = useState(null)
 
-    const getCollectionItems = async (values, { resetForm }) => {
+    const getCollectionItems = async () => {
         setLoading(true)
         try {
             const { data, error } = await supabase
@@ -23,13 +30,14 @@ export default function useGetCollectionItems() {
                 setLoading(false)
                 showToast('error', 'Error', 'Failed to fetch collection items')
                 Sentry.captureException(
-                    `Failed to fetch collection items in ${values.email}. ${cleanAuthError(error)}`
+                    `Failed to fetch collection items in ${user.email}. ${cleanAuthError(error)}`
                 )
                 console.log(error)
                 setData([])
             } else {
                 setLoading(false)
                 setData(data)
+                setCollectionItems(data)
             }
         } catch (error) {
             //setting state of user feedback stores to initialize user feedback components
@@ -37,7 +45,7 @@ export default function useGetCollectionItems() {
             await new Promise((resolve) => setTimeout(resolve, 100))
             showToast('error', 'Error', `Service not available right now.`)
             Sentry.captureException(
-                `Failed to fetch collection items in ${values.email}. Service not available right now. ${error}`
+                `Failed to fetch collection items in ${user.email}. Service not available right now. ${error}`
             )
         }
     }
